5c2eab6ef17b5530442ad67e6d14fa22729954a4
rithikb24 <rithik.bhandari@smallcase.com>
2024-04-23 01:32:36 +0530

    feat: structs done

--- main.go
+++ main.go
@@ -3,24 +3,82 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io/ioutil"
+	"net/http"
 	"os"
 )
 
-type Intro struct {
-	intro string    `json:"intro"`
-	story []Stories `json:"story"`
-}
+// type Intro struct {
+// 	intro string    `json:"intro"`
+// 	story []Stories `json:"story"`
+// }
 
-type Stories struct {
-	title   string    `json:"title"`
-	story   []string  `json:"story"`
-	options []Options `json:"options"`
-}
+// type Stories struct {
+// 	title   string    `json:"title"`
+// 	story   []string  `json:"story"`
+// 	options []Options `json:"options"`
+// }
 
-type Options struct {
-	text string `json:"text"`
-	arc  string `json:"arc"`
+// type Options struct {
+// 	text string `json:"text"`
+// 	arc  string `json:"arc"`
+// }
+
+type Adventure struct {
+	Intro struct {
+		Title   string   `json:"title"`
+		Story   []string `json:"story"`
+		Options []struct {
+			Text string `json:"text"`
+			Arc  string `json:"arc"`
+		} `json:"options"`
+	} `json:"intro"`
+	NewYork struct {
+		Title   string   `json:"title"`
+		Story   []string `json:"story"`
+		Options []struct {
+			Text string `json:"text"`
+			Arc  string `json:"arc"`
+		} `json:"options"`
+	} `json:"new-york"`
+	Debate struct {
+		Title   string   `json:"title"`
+		Story   []string `json:"story"`
+		Options []struct {
+			Text string `json:"text"`
+			Arc  string `json:"arc"`
+		} `json:"options"`
+	} `json:"debate"`
+	SeanKelly struct {
+		Title   string   `json:"title"`
+		Story   []string `json:"story"`
+		Options []struct {
+			Text string `json:"text"`
+			Arc  string `json:"arc"`
+		} `json:"options"`
+	} `json:"sean-kelly"`
+	MarkBates struct {
+		Title   string   `json:"title"`
+		Story   []string `json:"story"`
+		Options []struct {
+			Text string `json:"text"`
+			Arc  string `json:"arc"`
+		} `json:"options"`
+	} `json:"mark-bates"`
+	Denver struct {
+		Title   string   `json:"title"`
+		Story   []string `json:"story"`
+		Options []struct {
+			Text string `json:"text"`
+			Arc  string `json:"arc"`
+		} `json:"options"`
+	} `json:"denver"`
+	Home struct {
+		Title   string        `json:"title"`
+		Story   []string      `json:"story"`
+		Options []interface{} `json:"options"`
+	} `json:"home"`
 }
 
 func main() {
@@ -36,7 +94,12 @@ func main() {
 	defer jsonFile.Close()
 	byteAll, _ := ioutil.ReadAll(jsonFile)
 
-	var intro Intro
-	json.Unmarshal(byteAll, &intro)
-	fmt.Println(intro)
+	var adven Adventure
+	json.Unmarshal(byteAll, &adven)
+	fmt.Println(adven.Intro.Title)
+	tmpl := template.Must(template.ParseFiles("cyoa.html"))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tmpl.Execute(w, adven)
+	})
+	http.ListenAndServe(":80", nil)
 }
